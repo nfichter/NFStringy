dafa9d478dfd7093c012cb9fe0c7ee760ead92ed
nfichter <nfichter@stuy.edu>
2016-09-28 19:23:14 -0400

    cleaned up tests, finished strcmp

--- strfunctions.c
+++ strfunctions.c
@@ -43,16 +43,17 @@ char * nfstrncat(char *dest, char *source, int n) {
   return dest;
 }
 
-int strcmp(char *s1, char *s2) {
+int nfstrcmp(char *s1, char *s2) {
+  int len;
   if (nfstrlen(s1) < nfstrlen(s2)) {
-    len = s1;
+    len = nfstrlen(s1);
   } else {
-    len = s2;
+    len = nfstrlen(s2);
   }
-  lenCounter = len;
+  int lenCounter = len;
   while (lenCounter) {
     if (s1[lenCounter] != s2[lenCounter]) {
-      return s1[lenCounter]-s2[lenCounter];
+      return s1[len-lenCounter]-s2[len-lenCounter];
     }
     lenCounter--;
   }
@@ -61,16 +62,25 @@ int strcmp(char *s1, char *s2) {
  
 int main() {
   //strlen test - working
-  printf("\n");
+  printf("\n===strlen===\n");
   char s[] = "Hello";
   printf("s: %s\nstrlen(s): %d\n",s,nfstrlen(s));
+
   //strcpy test - working
-  printf("\n");
+  printf("\n===strcpy===\n");
   char s2[10];
   printf("s: %s\ns2 after copy: %s\n",s,nfstrcpy(s2,s));
+
   //strncat test - working
-  printf("\n");
+  printf("\n===strncat===\n");
   char s3[20] = "Hello there";
   printf("s3: %s\ns3 after ncat of s, n = 3: %s\n",s3,nfstrncat(s3,s,3));
+
   //strcmp test - to be written
+  printf("\n===strcmp===\n");
+  char s4[] = "Goodbye";
+  char s5[] = "Ok.";
+  printf("%s compared to %s: %d\n",s,s,nfstrcmp(s,s));
+  printf("%s compared to %s: %d\n",s,s4,nfstrcmp(s,s4));
+  printf("%s compared to %s: %d\n",s4,s5,nfstrcmp(s4,s5));
 }
